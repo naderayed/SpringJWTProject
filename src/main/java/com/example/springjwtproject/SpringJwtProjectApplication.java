4f14e1e5bcc82b67593f90e055d83899a9d19e0e
NaderAyed <nader.ayed@esprit.tn>
2022-11-27 14:17:07 +0100

    initial Configuration

---
+++ src/main/java/com/example/springjwtproject/SpringJwtProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjwtproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJwtProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springjwtproject/SpringJwtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjwtproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
